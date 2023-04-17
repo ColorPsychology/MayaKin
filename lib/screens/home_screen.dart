abb82b960c8c34911490f52efcb817f4926c4272
Nick(Seungyun Kim) <sy5072@naver.com>
2023-04-17 23:37:31 +0900

    [feat] 메인 스크린 상단 바 구현 (#1)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:mayakin/extensions/colors+.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text(
-        "Hi",
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 24,
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 60,
+          left: 20,
+          right: 20,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              "MayaKin",
+              style: TextStyle(
+                color: KinColors.textBlack100,
+                fontSize: 34,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                print("hi");
+              },
+              icon: const Icon(Icons.settings),
+              color: KinColors.grey50,
+              iconSize: 24,
+            )
+          ],
         ),
       ),
     );
