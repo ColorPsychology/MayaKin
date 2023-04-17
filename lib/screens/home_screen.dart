ede97257288a56f1621785c29bac3480986584e7
Nick(Seungyun Kim) <sy5072@naver.com>
2023-04-17 23:54:19 +0900

    [feat] 패딩 화면 전체 적용 (#1)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,45 +7,46 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 60,
-              left: 20,
-              right: 20,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  "MayaKin",
-                  style: TextStyle(
-                    color: KinColors.textBlack100,
-                    fontSize: 34,
-                    fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 60,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "MayaKin",
+                    style: TextStyle(
+                      color: KinColors.textBlack100,
+                      fontSize: 34,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    print("hi");
-                  },
-                  icon: const Icon(Icons.settings),
-                  color: KinColors.grey50,
-                  iconSize: 24,
-                )
-              ],
+                  IconButton(
+                    onPressed: () {
+                      print("hi");
+                    },
+                    icon: const Icon(Icons.settings),
+                    color: KinColors.grey50,
+                    iconSize: 24,
+                  )
+                ],
+              ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(
-              top: 20,
-              left: 20,
-              right: 20,
-            ),
-            child: AddButton(),
-          )
-        ],
+            const Padding(
+              padding: EdgeInsets.only(
+                top: 20,
+              ),
+              child: AddButton(),
+            )
+          ],
+        ),
       ),
     );
   }
