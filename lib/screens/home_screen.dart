1d6cbc01d4f1a6cd80aa49e15e23550d29ac69cd
Nick(Seungyun Kim) <sy5072@naver.com>
2023-04-17 23:06:45 +0900

    [add] homescreen 파일 생성 (#1)

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text(
+        "Hi",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 24,
+        ),
+      ),
+    );
+  }
+}
